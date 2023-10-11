80ad98a27f71ccdb8188ddd1ef04e4c846be26a8
nvatuu <tunv.todo@gmail.com>
2023-10-11 19:09:22 +0700

    add comment for linear regression

--- supervised_learning/l02_linear_regression.py
+++ supervised_learning/l02_linear_regression.py
@@ -5,11 +5,13 @@ import copy
 
 class MyLinearRegression:
     def __init__(self, lr, n_iter):
+        # Set hyper-parameters
         self.lr = lr
         self.n_iter = n_iter
         self.losses = []
 
     def initialize_weights(self, X):
+        # Initialize weights and biases
         n_features = X.shape[1]
         limit = 1 / math.sqrt(n_features)
         self.W = np.random.uniform(low=-limit, high=limit, size=(n_features, 1))
@@ -21,42 +23,52 @@ class MyLinearRegression:
         return X
 
     def fit(self, X, y):
-        # Prepare part
+        # Correct input format
         X = self._transform_x(X)
         X = self.padding_input(X)
         y = self._transform_y(y)
-        self.initialize_weights(X)
+
+        self.initialize_weights(X)                  # Initialize weights and biases
         origin_lr = self.lr
 
-        # Calculate part
+        # Training the model
+        # Epoch loop
         for i in range(self.n_iter):
             # Update learning rate
             self.lr = origin_lr / math.sqrt(i + 1)
-            # Calculate the output predict values
-            h = X @ self.W
-            # Calculate gradient, update weights
-            dW = self.gradient_mes_loss(X, h, y)
-            self.W -= self.lr * dW
-            # Calculate metrics
-            loss = self.compute_mse_loss(y, h)
+
+            # Forward pass
+            a = X @ self.W                          # Calculate the output predict values
+
+            # Compute the loss
+            loss = self.MSELoss(y, a)               # Mean Squared Error
             self.losses.append(loss)
+
+            # Backpropagation
+            dW = self.gradient_MSELoss(X, a, y)     # Calculate gradient
+
+            # Update weights
+            self.W -= self.lr * dW
+
+            # Print epoch result
             # print(f'Epoch {i}, lr = {self.lr}, mes_loss: {loss}')
 
-    def compute_mse_loss(self, y, h):
+    def MSELoss(self, y, h):
         # Mean Squared Error LOSS
         return np.sum(np.power(y - h, 2)) / y.shape[0]
 
-    def gradient_mes_loss(self, X, h, y):
+    def gradient_MSELoss(self, X, h, y):
         # Derivative of mean squared error LOSS
-        # Sum of gradient of the batch computation, must be divided by y.shape[0]
+        # Multiply result is sum of gradient of the batch computation => Must be divided by batch_len
         dW = X.transpose() @ (h - y) / y.shape[0]
         return dW
 
     def predict(self, X):
-        # Prepare part
+        # Correct input format
         X = self._transform_x(X)
         X = self.padding_input(X)
-        # Calculate part
+
+        # Forward pass / Calculate predicted output value
         return X @ self.W
 
     def _transform_x(self, x):
