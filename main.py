f1519b038df990d62f891653566af4aa0d0d64c2
tunvx <tunv.todo@gmail.com>
2023-12-19 16:44:37 +0700

    test05_mlp_binary_classifier

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
-from sklearn.datasets import load_breast_cancer, load_diabetes, load_iris, fetch_openml
+from sklearn.datasets import load_breast_cancer, load_diabetes, load_iris, load_digits
 from sklearn.linear_model import LogisticRegression, LinearRegression, LogisticRegressionCV
+from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, mean_squared_error, classification_report
 from sklearn.model_selection import train_test_split
@@ -97,7 +98,7 @@ def test04_knn_classifier_model():
 
 
 # TEST FOR LOGISTIC REGRESSION
-def test05_mlp_classification():
+def test05_mlp_binary_classifier():
     X, Y = load_planar_dataset()
     Y = Y.flatten()
     print(X.shape, Y.shape)
@@ -115,7 +116,7 @@ def test05_mlp_classification():
     print(f'With {n_epochs} epochs, accuracy my MLP model: {accuracy}')
     print(classification_report(y_te, y_pred))
 
-    model1 = LogisticRegressionCV()
+    model1 = MLPClassifier()
     model1.fit(X_tr, y_tr)
     y_pred = model1.predict(X_te)
     accuracy = accuracy_score(y_te, y_pred)
@@ -123,12 +124,10 @@ def test05_mlp_classification():
     print(classification_report(y_te, y_pred))
 
 
-
-
 if __name__ == "__main__":
     # test01_logistic_model()
     # test02_linear_model()
     # test03_multinomial_model()
     # test04_knn_classifier_model()
-    test05_mlp_classification()
+    test05_mlp_binary_classifier()
 
