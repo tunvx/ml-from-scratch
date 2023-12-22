58a4aa2a5032561a3e574c218c2405bec244cac1
tunvx <tunv.todo@gmail.com>
2023-12-22 16:23:01 +0700

    load catvnoncat data

--- dataset/load_data.py
+++ dataset/load_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import h5py
 
 
 def sklearn_to_df(data_loader):
@@ -21,3 +22,16 @@ def prepare_data_loader(X, y, batch_size):
         batch_y = y[start_idx:end_idx]
         yield batch_x, batch_y
 
+
+def load_catvnoncat_data():
+    train_dataset = h5py.File('data/catvnoncat/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('data/catvnoncat/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
